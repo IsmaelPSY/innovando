53cdae0607228bdb11201789a6071ed27090f298
Deskrel <ismaelpsy.12@gmail.com>
2023-12-16 16:46:46 -0500

    Adding netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "server", // activate server-side rendering
+  output: "server" // activate server-side rendering
+  ,
+  adapter: netlify()
 });
